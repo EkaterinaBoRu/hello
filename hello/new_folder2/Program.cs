e25ee8b80310edb1354abaa097281a352cd44c62
ekaterina <rudkate21@mail.ru>
2022-12-13 22:30:40 +0300

    маша

---
+++ hello/new_folder2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Hello, Маша!");
+}
+else
+{
+    Console.WriteLine("Hello");
+}
+
